0885d82b05373b7685303dd49f74ace147ef8ac3
manju <manjureddy23@gmail.com>
2021-07-03 20:16:05 +0530

    first commit

---
+++ src/test/java/com/paractice/mymaven/AppiumTest.java
@@ -0,0 +1,17 @@
+package com.paractice.mymaven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSapps() {
+		
+		System.out.println("IOSapps");
+	}
+
+}
---
+++ src/test/java/com/paractice/mymaven/RESTAPITest.java
@@ -0,0 +1,18 @@
+package com.paractice.mymaven;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira() {
+		
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+	
+}
